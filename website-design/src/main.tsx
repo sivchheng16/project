a63795db07aa669a2b8d07eb2f06b6b21883f2e5
sivchheng16 <kheangsivechheng@gmail.com>
2025-08-27 15:47:26 +0700

    main

--- website-design/src/main.tsx
+++ website-design/src/main.tsx
@@ -1,15 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import "./index.css";
 // import Nav from './component/Nav.jsx'
-import Header from './component/Header.jsx'
-import Hero from './component/Hero.js'
-import About from './component/About.js'
+import Header from "./component/Header.jsx";
+import Hero from "./component/Hero.js";
+import About from "./component/About.js";
+import Service from "./component/Service.js";
+import Portfolio from "./component/Portfolio.js";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <div>
     {/* <Nav /> */}
     <Header />
     <Hero />
     <About />
+    <Service />
+    <Portfolio />
   </div>
-)
+);
